c8660efb81a46b0843b9dabf6e39927f3e002f59
Arkadiy Vertleyb <vertleyb@hotmail.com>
2004-11-28 22:58:09 +0000

    documentation added
    [SVN r2374]

--- test/compliant/typeof_g++.bat
+++ test/compliant/typeof_g++.bat
@@ -1,2 +1,3 @@
 g++ -IC:\boost\boost_1_32_0 -I..\..\..\.. -D BOOST_TYPEOF_COMPLIANT -D BOOST_TYPEOF_LIMIT_SIZE=50 -D BOOST_MPL_LIMIT_VECTOR_SIZE=50 odr1.cpp odr2.cpp test_compliant.cpp ..\main.cpp 
 g++ -IC:\boost\boost_1_32_0 -I..\..\..\.. odr1.cpp odr2.cpp test_compliant.cpp ..\main.cpp 
+
