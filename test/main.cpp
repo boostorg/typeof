bc3ccb41da5f5feff15b126be593477118ba69ec
Arkadiy Vertleyb <vertleyb@hotmail.com>
2005-05-12 02:01:02 +0000

    minor fixes
    [SVN r2602]

--- test/main.cpp
+++ test/main.cpp
@@ -202,8 +202,8 @@ void lvalue_typeof_test()
     BOOST_STATIC_ASSERT((boost::is_same<BOOST_LVALUE_TYPEOF(rf()), int&>::value));
     BOOST_STATIC_ASSERT((boost::is_same<BOOST_LVALUE_TYPEOF(rcf()), const int&>::value));
     BOOST_STATIC_ASSERT((boost::is_same<BOOST_LVALUE_TYPEOF(cf()), const int&>::value));
-    //BOOST_STATIC_ASSERT((boost::is_same<BOOST_LVALUE_TYPEOF(21), int>::value));
-    //BOOST_STATIC_ASSERT((boost::is_same<BOOST_LVALUE_TYPEOF(int(21)), int>::value));
+    //BOOST_STATIC_ASSERT((boost::is_same<BOOST_LVALUE_TYPEOF(21), int>::value)); //msvc
+    BOOST_STATIC_ASSERT((boost::is_same<BOOST_LVALUE_TYPEOF(int(21)), int>::value));
 }
 
 #define BOOST_TYPEOF_TEXT "Noncopyable..."
