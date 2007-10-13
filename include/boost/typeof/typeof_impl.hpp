d5c39b3615412af75b8f1dbaad65e12941b2cdbd
Daniel James <daniel@calamity.org.uk>
2007-10-13 23:18:35 +0000

    Merge with the offending files removed.
    [SVN r39995]

--- include/boost/typeof/typeof_impl.hpp
+++ include/boost/typeof/typeof_impl.hpp
@@ -92,7 +92,7 @@ namespace boost { namespace type_of {
 
 #define BOOST_TYPEOF_NESTED_TYPEITEM(z, n, expr)\
     BOOST_STATIC_CONSTANT(int,BOOST_PP_CAT(value,n) = sizeof(boost::type_of::encode<_typeof_start_vector>(expr).item ## n));\
-    typedef boost::mpl::size_t<BOOST_PP_CAT(value,n)> BOOST_PP_CAT(item,n);
+    typedef boost::mpl::size_t<BOOST_PP_CAT(self_t::value,n)> BOOST_PP_CAT(item,n);
 
 #ifdef __DMC__
 #define BOOST_TYPEOF_NESTED_TYPEITEM_2(z,n,expr)\
@@ -103,26 +103,60 @@ namespace boost { namespace type_of {
     typedef _typeof_fraction_iter<Pos> fraction_type;
 #else
 #define BOOST_TYPEOF_FRACTIONTYPE()\
-    typedef _typeof_encode_fraction<iteration> fraction_type;
+    typedef _typeof_encode_fraction<self_t::iteration> fraction_type;
 #endif
 
+#ifdef __BORLANDC__
+namespace boost { namespace type_of {
+    template<typename Pos,typename Iter>
+    struct generic_typeof_fraction_iter {
+        typedef generic_typeof_fraction_iter<Pos,Iter> self_t;
+        static const int pos=(Pos::value);
+        static const int iteration=(pos/5);
+        static const int where=pos%5;
+        typedef typename Iter::template _apply_next<self_t::iteration>::type fraction_type;
+        typedef generic_typeof_fraction_iter<typename Pos::next,Iter> next;
+        typedef typename v_iter<fraction_type,mpl::int_<self_t::where> >::type type;
+    };
+}}
 #define BOOST_TYPEOF_NESTED_TYPEDEF_IMPL(expr) \
         template<int _Typeof_Iteration>\
         struct _typeof_encode_fraction {\
+            typedef _typeof_encode_fraction<_Typeof_Iteration> self_t;\
             BOOST_STATIC_CONSTANT(int,_typeof_encode_offset = (_Typeof_Iteration*BOOST_TYPEOF_LIMIT_SIZE));\
-            typedef boost::type_of::offset_vector<BOOST_TYPEOF_VECTOR(0)<>,boost::mpl::size_t<_typeof_encode_offset> > _typeof_start_vector;\
+            typedef boost::type_of::offset_vector<BOOST_TYPEOF_VECTOR(0)<>,boost::mpl::size_t<self_t::_typeof_encode_offset> > _typeof_start_vector;\
+            BOOST_PP_REPEAT(BOOST_TYPEOF_LIMIT_SIZE,BOOST_TYPEOF_NESTED_TYPEITEM,expr)\
+            template<int Next>\
+            struct _apply_next {\
+                typedef _typeof_encode_fraction<Next> type;\
+            };\
+        };\
+        template<typename Pos>\
+        struct _typeof_fraction_iter {\
+            typedef boost::type_of::generic_typeof_fraction_iter<Pos,_typeof_encode_fraction<0> > self_t;\
+            typedef typename self_t::next next;\
+            typedef typename self_t::type type;\
+        };
+#else
+#define BOOST_TYPEOF_NESTED_TYPEDEF_IMPL(expr) \
+        template<int _Typeof_Iteration>\
+        struct _typeof_encode_fraction {\
+            typedef _typeof_encode_fraction<_Typeof_Iteration> self_t;\
+            BOOST_STATIC_CONSTANT(int,_typeof_encode_offset = (_Typeof_Iteration*BOOST_TYPEOF_LIMIT_SIZE));\
+            typedef boost::type_of::offset_vector<BOOST_TYPEOF_VECTOR(0)<>,boost::mpl::size_t<self_t::_typeof_encode_offset> > _typeof_start_vector;\
             BOOST_PP_REPEAT(BOOST_TYPEOF_LIMIT_SIZE,BOOST_TYPEOF_NESTED_TYPEITEM,expr)\
         };\
         template<typename Pos>\
         struct _typeof_fraction_iter {\
+            typedef _typeof_fraction_iter<Pos> self_t;\
             BOOST_STATIC_CONSTANT(int,pos=(Pos::value));\
             BOOST_STATIC_CONSTANT(int,iteration=(pos/BOOST_TYPEOF_LIMIT_SIZE));\
             BOOST_STATIC_CONSTANT(int,where=pos%BOOST_TYPEOF_LIMIT_SIZE);\
             BOOST_TYPEOF_FRACTIONTYPE()\
-            typedef typename boost::type_of::v_iter<fraction_type,boost::mpl::int_<where> >::type type;\
+            typedef typename boost::type_of::v_iter<fraction_type,boost::mpl::int_<self_t::where> >::type type;\
             typedef _typeof_fraction_iter<typename Pos::next> next;\
         };
-
+#endif
 #ifdef __MWERKS__
 
 # define BOOST_TYPEOF_NESTED_TYPEDEF(name,expr) \
